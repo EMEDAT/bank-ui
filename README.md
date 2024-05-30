2418b4d5d8ba36a6c01a46dc23dd1cf906bb2212
Emediong David Titus <emedattitus24@gmail.com>
2024-05-30 03:23:31 +0100

    updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# bank-ui
-Project Hoobank UI
+# bank-ui - Modern UI/UX website using React.js & Tailwind CSS
+Project Hoobank UI 
+
+![HooBank](https://i.ibb.co/BK1Hn0x/Screenshot-2022-08-08-at-4-05-48-PM.png)
---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Ditch the credit card confusion! Our service helps you find the perfect card deal in just a few easy steps.  Say goodbye to endless searching – answer some simple questions and we'll match you with top offers that fit your spending habits. Get rewarded for what you already buy!
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
